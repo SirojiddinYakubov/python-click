4cd2a5ab58f6bd36a7f7336983b42e83f45082d4
Sirojiddin Yakubov <yakubov9791999@gmail.com>
2022-10-05 11:16:56 +0500

    nc

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if sys.argv[-1] == 'publish':
 
 PACKAGE = "pyclick"
 PACKAGE_NAME = "python-click"
-VERSION = '0.0.11'
+VERSION = '0.0.12'
 DESCRIPTION = 'Этот пакет позволяет легко интегрировать платежную систему Click через фреймворк Django'
 
 
